985c0c605476e9ae38287752b35d90823c787d34
iamusayev <anarmusayev1337@gmail.com>
2023-01-20 13:14:49 +0400

    Added some classes

--- src/main/java/az/iamusayev/ApplicationRunner.java
+++ src/main/java/az/iamusayev/ApplicationRunner.java
@@ -1,18 +1,17 @@
 package az.iamusayev;
 
+import az.iamusayev.spring.database.entity.Company;
 import az.iamusayev.spring.database.pool.ConnectionPool;
 import az.iamusayev.spring.database.repository.CompanyRepository;
+import az.iamusayev.spring.database.repository.CrudRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml")) {
-            var connectionPool = context.getBean("p2", ConnectionPool.class);
-            System.out.println(connectionPool);
-
-            CompanyRepository companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-            System.out.println(companyRepository);
+            CrudRepository companyRepository = context.getBean("companyRepository",CrudRepository.class);
+            System.out.println(companyRepository.findById(1));
         }
     }
 }
--- src/main/java/az/iamusayev/spring/database/repository/CompanyRepository.java
+++ src/main/java/az/iamusayev/spring/database/repository/CompanyRepository.java
@@ -1,5 +1,6 @@
 package az.iamusayev.spring.database.repository;
 
+import az.iamusayev.spring.bpp.Auditing;
 import az.iamusayev.spring.bpp.InjectBean;
 import az.iamusayev.spring.bpp.Transaction;
 import az.iamusayev.spring.database.entity.Company;
@@ -7,26 +8,32 @@ import az.iamusayev.spring.database.pool.ConnectionPool;
 import java.util.Optional;
 import javax.annotation.PostConstruct;
 
+
+@Auditing
 @Transaction
 public class CompanyRepository implements CrudRepository<Integer, Company> {
-
 
     @InjectBean
     private ConnectionPool connectionPool;
 
     @PostConstruct
-    private void init() {
+    public void init() {
         System.out.println("init company repository");
     }
 
+
     @Override
     public Optional<Company> findById(Integer id) {
-        System.out.println("findById method");
+        System.out.println("findById method....");
         return Optional.of(new Company(id));
     }
 
     @Override
     public void delete(Company entity) {
-        System.out.println("delete method");
+        System.out.println("delete method....");
     }
 }
+
+
+
+
