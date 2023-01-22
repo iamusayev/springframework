ae155e962b8eb7de917a2f46e1c59aed3d4195a2
iamusayev <anarmusayev1337@gmail.com>
2023-01-22 13:41:02 +0400

    commit java based configuration

--- src/main/java/az/iamusayev/ApplicationRunner.java
+++ src/main/java/az/iamusayev/ApplicationRunner.java
@@ -1,14 +1,19 @@
 package az.iamusayev;
 
+import az.iamusayev.spring.config.ApplicationConfiguration;
 import az.iamusayev.spring.database.pool.ConnectionPool;
 import az.iamusayev.spring.database.repository.CrudRepository;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml")) {
-            var connectionPool = context.getBean(ConnectionPool.class);
+        try (var context = new AnnotationConfigApplicationContext()) {
+            context.register(ApplicationConfiguration.class);
+            context.getEnvironment().setActiveProfiles("web", "prod");
+            context.refresh();
+            var connectionPool = context.getBean("pool1");
             System.out.println(connectionPool);
 
             var companyRepository = context.getBean(CrudRepository.class);
---
+++ src/main/java/az/iamusayev/spring/config/ApplicationConfiguration.java
@@ -0,0 +1,59 @@
+package az.iamusayev.spring.config;
+
+import az.iamusayev.spring.database.pool.ConnectionPool;
+import az.iamusayev.spring.database.repository.CrudRepository;
+import az.iamusayev.spring.database.repository.UserRepository;
+import az.iamusayev.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@ImportResource("classpath:application.xml")
+@Import(WebConfiguration.class)
+@Configuration(proxyBeanMethods = true)
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "az.iamusayev.spring",
+        useDefaultFilters = false,
+        includeFilters = {
+                @Filter(type = FilterType.ANNOTATION, value = Component.class),
+                @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
+                @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
+        })
+public class ApplicationConfiguration {
+
+    @Bean
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool2(@Value("${db.username}") String username) {
+        return new ConnectionPool(username, 20);
+    }
+
+    @Bean
+    public ConnectionPool pool3() {
+        return new ConnectionPool("test-pool", 25);
+    }
+
+    @Bean
+    @Profile("prod|web")
+//    ! & |
+    public UserRepository userRepository2(ConnectionPool pool2) {
+        return new UserRepository(pool2);
+    }
+
+    @Bean
+    public UserRepository userRepository3() {
+        ConnectionPool connectionPool1 = pool3();
+        ConnectionPool connectionPool2 = pool3();
+        ConnectionPool connectionPool3 = pool3();
+        return new UserRepository(pool3());
+    }
+}
